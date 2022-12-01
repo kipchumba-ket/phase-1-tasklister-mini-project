e6ebde646a5eef65bb1204d4f7a68b477a0b4d9b
kipchumba-ket <kipchumbaelly97@gmail.com>
2022-12-01 13:29:19 +0300

    completed assignmnet

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+let form = document.getElementById('create-task-form')
+form.addEventListener('submit',(e) => {
+  e.preventDefault()
+  makeTask(e.target['new-task-description'].value)
+  form.reset()
+})
 });
+
+
+function makeTask (task){
+let li = document.createElement('li');
+let button = document.createElement('button')
+button.addEventListener('click', deleteTask)
+button.textContent = "DELETE"
+li.textContent = task + " "
+li.appendChild(button)
+document.getElementById("tasks").appendChild(li)
+}
+
+
+function deleteTask(e){
+e.target.parentNode.remove()
+}
